f07ec1dbf8ba888725565d6cf3a25b23027a71a0
Khaled.K <3eeqeel@gmail.com>
2020-01-06 10:07:03 +0300

    Update tree.h

--- include/lightdata/tree.h
+++ include/lightdata/tree.h
@@ -4,9 +4,9 @@
 
 typedef _BTree; typedef struct _BTree{ int k; void* d; _BTree* l,r; } BTree;
 
-int btree_max(int a, int b){ return a > b ? a : b; }
-int btree_size(BTree** t){ return *t ? btree_size((*t)->l) + btree_size((*t)->r) : 0; }
-int btree_depth(BTree** t){ return *t ? btree_max(btree_depth((*t)->l), btree_depth((*t)->r)) + 1 : 0; }
+int int_max(int a, int b){ return a > b ? a : b; }
+int btree_size(BTree** t){ return *t ? 1 + btree_size((*t)->l) + btree_size((*t)->r) : 0; }
+int btree_depth(BTree** t){ return *t ? int_max(btree_depth((*t)->l), btree_depth((*t)->r)) + 1 : 0; }
 
 void btree_insert(BTree** t, int k, void* d)
 {
